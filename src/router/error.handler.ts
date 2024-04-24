32f5eccc9ae10de1c8816b8cfadb76886a98b7dc
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-04-24 21:06:29 +0400

    Add Custom Error

--- src/router/error.handler.ts
+++ src/router/error.handler.ts
@@ -1,16 +1,13 @@
 import { validationResult } from "express-validator";
 import { Request, Response } from "express";
 
-export async function validateRequest(req: Request, res: Response) {
-    try {  
-        const errors =  validationResult(req);
-        console.log("222");
-        if (!errors.isEmpty()) {
-            console.log(errors);
-            throw new Error("Bad request");
-        }
-    } catch (error) {
-        console.log("333");
-        console.log(error)
+
+export function validateRequest(req: Request, res: Response, next: Function): void {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        throw new Error("Bad request");
     }
+    next();
 }
--- src/servcice/signup.service.ts
+++ src/servcice/signup.service.ts
@@ -1,15 +1,16 @@
 import { User } from "@prisma/client";
 import { prismaClient } from "../../prisma/client";
 import bcrypt from "bcrypt";
+import { BadRequestError } from "./badrequestError.handler";
 
 export class SignUpService {
-    public static async signUp(userName: string, password: string, email: string): Promise<User | undefined> {
+    public static async signUp(userName: string, password: string, email: string): Promise<User | undefined | Error> {
         try {
             const userRepo = prismaClient.user;
             const hashedPassword = await bcrypt.hash(password, 10);
             const user: User | null = await userRepo.findUnique({ where: { email } });
             if (user) {
-                throw new Error("User already exists");
+                throw new BadRequestError("User already exists");
             }
             const userCreated: User = await userRepo.create({
                 data: {
@@ -20,7 +21,8 @@ export class SignUpService {
             });
             return userCreated;
         } catch (error) {
-            console.log(error);
+            console.log(555, error)
+            return <Error>error;
         }
     }
 }
